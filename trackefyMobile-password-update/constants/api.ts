aa3eef1138b07ee3f1f2e5de34b473d8981c4c63
Rhitwik <chitwik@gmail.com>
2026-08-30 09:38:55 +0530

    user experience in account creation

--- trackefyMobile-password-update/constants/api.ts
+++
@@ -1,6 +0,0 @@
-// export const BASE_URL = "https://kidharhaibus-backend-production.up.railway.app";
-const LOCAL = "http://192.168.29.190:5000/api";
-const PROD = "https://api.trackefy.in/api";
-
-// 👇 switch here
-export const BASE_URL = PROD;
--- trackefyMobile-password-update/firebase.js
+++
@@ -1,8 +0,0 @@
-import { initializeApp, getApps } from '@react-native-firebase/app';
-
-// This ensures Firebase is initialized only once
-if (getApps().length === 0) {
-  initializeApp();
-}
-
-export default null;
